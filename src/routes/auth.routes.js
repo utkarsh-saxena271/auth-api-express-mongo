e97789cfb4873b2be0d6e42b551a6adb3fbe460e
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2025-09-02 18:26:42 +0530

    started with jwt auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken'); 
 
 const router = express.Router()
 
@@ -9,10 +10,32 @@ router.post('/register',async (req,res)=>{
     const user  = await userModel.create({
         username,password
     })
+
+    const token = jwt.sign({
+        id:user._id,
+
+    },process.env.JWT_SECRET)
+
     res.status(201).json({
-        message:"user registered successfully"
+        message:"user registered successfully",
+        user,
+        token
     })
 })
+
+router.get("/user",async (req,res)=>{
+    const {token} = req.body
+
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET )
+})
+
+
 router.post("/login",async (req,res)=>{
     const {username, password} = req.body
 
